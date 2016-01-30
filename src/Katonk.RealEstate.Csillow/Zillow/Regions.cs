c9818657cab5709a2550fb41d6a16497a60aed48
RT <ryley_taketa@hotmail.com>
2016-01-30 06:47:37 -0800

    Update the regions type to convert fields to ints.

--- src/Katonk.RealEstate.Csillow/Zillow/Regions.cs
+++ src/Katonk.RealEstate.Csillow/Zillow/Regions.cs
@@ -12,29 +12,29 @@ namespace Katonk.RealEstate.Zillow
     [System.Xml.Serialization.XmlTypeAttribute("Regions", Namespace = "http://www.zillow.com/static/xsd/ZillowTypes.xsd")]
     public partial class Regions
     {
-        [System.Xml.Serialization.XmlElementAttribute("zipcode-id", Form = System.Xml.Schema.XmlSchemaForm.Unqualified, DataType = "integer")]
-        public string ZipCodeId
+        [System.Xml.Serialization.XmlElementAttribute("zipcode-id", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public int ZipCodeId
         {
             get;
             set;
         }
 
-        [System.Xml.Serialization.XmlElementAttribute("city-id", Form = System.Xml.Schema.XmlSchemaForm.Unqualified, DataType = "integer")]
-        public string CityId
+        [System.Xml.Serialization.XmlElementAttribute("city-id", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public int CityId
         {
             get;
             set;
         }
 
-        [System.Xml.Serialization.XmlElementAttribute("county-id", Form = System.Xml.Schema.XmlSchemaForm.Unqualified, DataType = "integer")]
-        public string CountyId
+        [System.Xml.Serialization.XmlElementAttribute("county-id", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public int CountyId
         {
             get;
             set;
         }
 
-        [System.Xml.Serialization.XmlElementAttribute("state-id", Form = System.Xml.Schema.XmlSchemaForm.Unqualified, DataType = "integer")]
-        public string StateId
+        [System.Xml.Serialization.XmlElementAttribute("state-id", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public int StateId
         {
             get;
             set;
